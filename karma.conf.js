7b3b46301bd20e327eb8c0144acf618fa7751c44
devopMC <devops@mc.com.tn>
2024-05-14 11:23:08 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,39 @@
+// karma.conf.js
 module.exports = function(config) {
   config.set({
+    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // for example, you can disable the random execution of tests
+        // or set a specific seed value
+        // options: {
+        //   random: false,
+        //   seed: '4321',
+        //   oneFailurePerSpec: true,
+        //   failFast: true
+        // }
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/your-parent'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
