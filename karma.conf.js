a71e24b76ae3ad90712c58388e33ff956093c325
devopMC <devops@mc.com.tn>
2024-05-17 11:26:06 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,14 @@
-// Karma configuration
-// Generated on Fri May 17 2024 10:59:37 GMT+0100 (Central European Standard Time)
-
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+    // Base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
+    // Frameworks to use
+    // Available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
- plugins: [
+
+    plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
@@ -19,64 +16,58 @@ module.exports = function(config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
 
- client: {
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    // list of files / patterns to load in the browser
-    files: [src/app/app.component.spec.ts
+
+    // List of files / patterns to load in the browser
+    files: [
+      'src/**/*.spec.ts'
     ],
 
+    // List of files / patterns to exclude
+    exclude: [],
 
-    // list of files / patterns to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
-    preprocessors: {src/main.ts
+    // Preprocess matching files before serving them to the browser
+    // Available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
+    preprocessors: {
+      'src/**/*.spec.ts': ['@angular-devkit/build-angular']
     },
 
+    // Test results reporter to use
+    // Possible values: 'dots', 'progress'
+    // Available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    reporters: ['progress', 'kjhtml'],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
+    // Web server port
     port: 9876,
 
-
-    // enable / disable colors in the output (reporters and logs)
+    // Enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // Level of logging
+    // Possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
-    // enable / disable watching file and executing tests whenever any file changes
+    // Enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
-    // start these browsers
-    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ['Chrome'],
-
+    // Start these browsers
+    // Available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    // If true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
     // Concurrency level
-    // how many browser instances should be started simultaneously
+    // How many browser instances should be started simultaneously
     concurrency: Infinity
   })
 }
